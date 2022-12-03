e2e66559fa7a7f3a29c563808ed6247e2ecb12ea
Gustavo Santos <gustavosmc@ufrrj.br>
2022-12-03 03:28:22 -0300

    Added Colors

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,19 @@ use std::thread;
 use sdl2::{pixels::Color, event::Event};
 use std::time::Duration;
 use sdl2::rect::{Rect};
-use rand::{thread_rng, Rng};
+
+fn calculate_color(x_position: f32, y_position : f32, size : u32) -> Color{
+    let new_size = (size / 2) as f32;
+    let red : f32;
+    let green : f32;
+    let blue : f32;
+
+    red = (1.0 - (((x_position - new_size).abs()) / new_size)) * 255.0;
+    green = (1.0 - (((y_position - new_size).abs()) / new_size)) * 255.0;
+    blue = (1.0 - (((((x_position + y_position) / 2.0) - new_size).abs()) / new_size)) * 255.0;
+
+    return Color::RGB(red as u8, green as u8, blue as u8); 
+}
 
 fn main (){
     // Create sdl context
@@ -37,9 +49,6 @@ fn main (){
     // Keybord controller
     let mut event_log = sdl_context.event_pump().unwrap();
 
-    // Random number
-    let mut rng = thread_rng();
-
     'main_loop: loop{
         // Cleaning the canvas with black
         canvas.set_draw_color(Color::RGB(0, 0, 0));
@@ -54,15 +63,15 @@ fn main (){
                     let x_position = (columns as i32) * rect_size;
                     let y_position = (rows as i32) * rect_size;
                     let alive_rect = Rect::new(x_position, y_position, rectangle_size, rectangle_size);
+                    canvas.set_draw_color(calculate_color(x_position as f32, y_position as f32, window_size));
                     canvas.draw_rect(alive_rect);
-                    canvas.fill_rect(alive_rect);
                 }
             }
         }
         
         // Showing the canvas.
         canvas.present();
-        thread::sleep(Duration::new(0, 1800000));
+        thread::sleep(Duration::new(0, 6000000));
 
         // Computes the next cylce
         matrix = compute::compute_matrix(matrix);
